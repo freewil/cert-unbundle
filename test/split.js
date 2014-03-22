eef477decd04ff4d670b178cc2d61080b46cbfca
James Halliday <mail@substack.net>
2014-03-21 19:55:50 -0700

    array-map for ie8

--- test/split.js
+++ test/split.js
@@ -1,12 +1,13 @@
 var test = require('tape');
 var unbundle = require('../');
+var map = require('array-map');
 
 var certs = [
     '-----BEGIN CERTIFICATE-----\nABCDEFG\n-----END CERTIFICATE-----',
     '\n-----BEGIN CERTIFICATE-----\nQRS\n-----END CERTIFICATE-----\n',
     '\r\nbeep\r\n-----BEGIN CERTIFICATE-----\nXYZ\n-----END CERTIFICATE----- '
 ];
-var expected = certs.map(function (s) {
+var expected = map(certs, function (s) {
     return '-----BEGIN ' + s.split(/-----BEGIN /)[1].replace(/^\s+|\s+$/, '');
 });
 var bundle = expected.join('\n');
